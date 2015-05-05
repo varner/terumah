d783fa88152813c31bdc757fc362c202475d846a
Madeleine Varner <maddyvarner@gmail.com>
2015-05-05 14:28:30 -0400

    🎾🎾🎾🎾🎾

--- js/terumah.js
+++ js/terumah.js
@@ -1,2 +1,28 @@
 // terumah.js
 // by xX_mAdDy_VaRnEr_Xx
+
+var page = min = 1;
+var max = 11;
+var delay = 500;
+
+$( document ).ready(function() {
+    $( "#down" ).click(function() {
+        console.log("down click");
+        if (page != max) {
+            page++;
+            $('html,body').animate({
+              scrollTop: $( "#" + page.toString() ).offset().top
+            }, delay);
+        }
+    });
+    
+    $( "#up" ).click(function() {
+        console.log("up click");
+        if (page != min) {
+            page--;
+            $('html,body').animate({
+              scrollTop: $( "#" + page.toString() ).offset().top
+            }, delay);
+        }
+    });
+});
